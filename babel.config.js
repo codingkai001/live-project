7a00ac7d9ee7425df489fe2e7b594d4e3e35ec15
temporaryforfzuse <temporaryforfzuse@zsxsoft.com>
2019-04-22 10:31:21 +0800

    Initialize Vuejs Repo

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,21 @@
 module.exports = {
+<<<<<<< HEAD
   presets: [
     '@vue/app'
   ]
 }
+=======
+  "presets": [
+    "@vue/app"
+  ],
+  "plugins": [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
+  ]
+}
+>>>>>>> Initialize Vuejs Repo
